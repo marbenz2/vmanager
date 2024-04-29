86e274087bfa6d42022f9fcb4b231ff61ba190fe
marbenz2 <benzinger.marcel@gmail.com>
2024-04-29 09:56:22 +0200

    style changed size of max-width

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import WalletComponent from "./components/WalletComponent";
 
 function App() {
   return (
-    <div className="flex flex-col w-full max-w-7xl gap-1">
+    <div className="flex flex-col w-full max-w-4xl gap-1">
       <section className="flex flex-col w-full gap-1">
         <Title className="text-3xl" title={"VManager"} />
       </section>
@@ -19,7 +19,7 @@ function App() {
         <WalletComponent />
         <AccountInfo />
       </section>
-      <section className="flex w-full">{/* <Nfts /> */}</section>
+      {/* <section className="flex w-full"><Nfts /></section> */}
     </div>
   );
 }
